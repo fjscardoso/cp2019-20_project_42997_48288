3c316ea28f84c62ac8c4886b1e99e40d410c7770
João Lourenço <joao.lourenco@fct.unl.pt>
2020-03-30 20:39:47 +0100

    Fixed implementation of reduce and scan. Replaced syntax "pointer + value" to "&pointer[value]"

--- code/src/patterns.c
+++ code/src/patterns.c
@@ -7,8 +7,8 @@ void map (void *dest, void *src, size_t nJob, size_t sizeJob, void (*worker)(voi
     assert (dest != NULL);
     assert (src != NULL);
     assert (worker != NULL);
-    for (int i=0; i < nJob; i++) {
-        worker(dest + i * sizeJob, src + i * sizeJob);
+    for (int i = 0;  i < nJob;  i++) {
+        worker (&dest[i * sizeJob], &src[i * sizeJob]);
     }
 }
 
@@ -17,10 +17,10 @@ void reduce (void *dest, void *src, size_t nJob, size_t sizeJob, void (*worker)(
     assert (dest != NULL);
     assert (src != NULL);
     assert (worker != NULL);
-    if (nJob > 1) {
-        memcpy (dest, src, sizeJob);
+    if (nJob > 0) {
+        memcpy (&dest[0], &src[0], sizeJob);
         for (int i = 1;  i < nJob;  i++)
-            worker(dest, dest, src + i * sizeJob);
+            worker (&dest[0], &dest[0], &src[i * sizeJob]);
     }
 }
 
@@ -30,9 +30,9 @@ void scan (void *dest, void *src, size_t nJob, size_t sizeJob, void (*worker)(vo
     assert (src != NULL);
     assert (worker != NULL);
     if (nJob > 1) {
-        memcpy (dest, src, sizeJob);
+        memcpy (&dest[0], &src[0], sizeJob);
         for (int i = 1;  i < nJob;  i++)
-            worker(dest + i * sizeJob, src + i * sizeJob, dest + (i-1) * sizeJob);
+            worker (&dest[i * sizeJob], &dest[(i-1) * sizeJob], &src[i * sizeJob]);
     }
 }
 
@@ -41,7 +41,7 @@ int pack (void *dest, void *src, size_t nJob, size_t sizeJob, const int *filter)
     int pos = 0;
     for (int i=0; i < nJob; i++) {
         if (filter[i]) {
-            memcpy (dest + pos * sizeJob, src + i * sizeJob, sizeJob);
+            memcpy (&dest[pos * sizeJob], &src[i * sizeJob], sizeJob);
             pos++;
         }
     }
@@ -51,27 +51,27 @@ int pack (void *dest, void *src, size_t nJob, size_t sizeJob, const int *filter)
 void gather (void *dest, void *src, size_t nJob, size_t sizeJob, const int *filter, int nFilter) {
     /* To be implemented */
     for (int i=0; i < nFilter; i++) {
-        memcpy (dest + i * sizeJob, src + filter[i] * sizeJob, sizeJob);
+        memcpy (&dest[i * sizeJob], &src[filter[i] * sizeJob], sizeJob);
     }
 }
 
 void scatter (void *dest, void *src, size_t nJob, size_t sizeJob, const int *filter) {
     /* To be implemented */
     for (int i=0; i < nJob; i++) {
-        memcpy (dest + filter[i] * sizeJob, src + i * sizeJob, sizeJob);
+        memcpy (&dest[filter[i] * sizeJob], &src[i * sizeJob], sizeJob);
     }
 }
 
 void pipeline (void *dest, void *src, size_t nJob, size_t sizeJob, void (*workerList[])(void *v1, const void *v2), size_t nWorkers) {
     /* To be implemented */
     for (int i=0; i < nJob; i++) {
-        memcpy (dest + i * sizeJob, src + i * sizeJob, sizeJob);
+        memcpy (&dest[i * sizeJob], &src[i * sizeJob], sizeJob);
         for (int j = 0;  j < nWorkers;  j++)
-            workerList[j](dest + i * sizeJob, dest + i * sizeJob);
+            workerList[j] (&dest[i * sizeJob], &dest[i * sizeJob]);
     }
 }
 
 void farm (void *dest, void *src, size_t nJob, size_t sizeJob, void (*worker)(void *v1, const void *v2), size_t nWorkers) {
     /* To be implemented */
-    map (dest, src, nJob, sizeJob, worker);
+    map (dest, src, nJob, sizeJob, worker);  // it provides the right result, but is a very very vey bad implementation…
 }
